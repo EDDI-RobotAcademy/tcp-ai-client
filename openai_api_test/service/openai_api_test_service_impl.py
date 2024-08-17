c65b8db46507e2135a5bef0ab8fbf4d0be4eb0f6
Jaehyuk Han <jh.jaehyuk213@gmail.com>
2024-08-17 11:43:08 +0900

    feat: [한재혁] OpenAI API 사용자 정의 프로토콜 등록 [TCPAI-2]

---
+++ openai_api_test/service/openai_api_test_service_impl.py
@@ -0,0 +1,23 @@
+from openai_api_test.repository.openai_api_test_repository_impl import OpenaiApiTestRepositoryImpl
+from openai_api_test.service.openai_api_test_service import OpenaiApiTestService
+
+
+class OpenaiApiTestServiceImpl(OpenaiApiTestService):
+    __instance = None
+
+    def __new__(cls):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+            cls.__instance.__openaiApiTestRepositoryImpl = OpenaiApiTestRepositoryImpl.getInstance()
+
+        return cls.__instance
+
+    @classmethod
+    def getInstance(cls):
+        if cls.__instance is None:
+            cls.__instance = cls()
+
+        return cls.__instance
+
+    def letsChat(self, userSendMessage):
+        return self.__openaiApiTestRepositoryImpl.generateText(userSendMessage)
--- user_defined_protocol/register.py
+++ user_defined_protocol/register.py
@@ -7,6 +7,9 @@ from first_user_defined_function_domain.service.response.fudf_just_for_test_resp
 from llama_test.service.llama_test_service_impl import LlamaTestServiceImpl
 from llama_test.service.request.llama_test_request import LlamaTestRequest
 from llama_test.service.response.llama_test_response import LlamaTestResponse
+from openai_api_test.service.openai_api_test_service_impl import OpenaiApiTestServiceImpl
+from openai_api_test.service.request.openai_api_test_request import OpenaiApiTestRequest
+from openai_api_test.service.response.openai_api_test_response import OpenaiApiTestResponse
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'template'))
 
@@ -64,6 +67,29 @@ class UserDefinedProtocolRegister:
         )
 
     @staticmethod
+    def registerOpenaiApiTestProtocol():
+        customProtocolService = CustomProtocolServiceImpl.getInstance()
+        openaiApiTestService = OpenaiApiTestServiceImpl.getInstance()
+
+        requestClassMapInstance = RequestClassMap.getInstance()
+        requestClassMapInstance.addRequestClass(
+            UserDefinedProtocolNumber.TCP_TEAM_OPENAI_TEST,
+            OpenaiApiTestRequest
+        )
+
+        responseClassMapInstance = ResponseClassMap.getInstance()
+        responseClassMapInstance.addResponseClass(
+            UserDefinedProtocolNumber.TCP_TEAM_OPENAI_TEST,
+            OpenaiApiTestResponse
+        )
+
+        customProtocolService.registerCustomProtocol(
+            UserDefinedProtocolNumber.TCP_TEAM_OPENAI_TEST,
+            openaiApiTestService.letsChat
+        )
+
+    @staticmethod
     def registerUserDefinedProtocol():
         UserDefinedProtocolRegister.registerDefaultUserDefinedProtocol()
         UserDefinedProtocolRegister.registerLlamaTestProtocol()
+        UserDefinedProtocolRegister.registerOpenaiApiTestProtocol()
