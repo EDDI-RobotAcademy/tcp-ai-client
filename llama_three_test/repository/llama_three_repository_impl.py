3fbe3c65c6a0a1bb3974bf7221b2122ff550443e
Jaehyuk Han <jh.jaehyuk213@gmail.com>
2024-08-18 11:40:04 +0900

    fix: [한재혁] 생성 요청할 때마다 LLaMA3 모델이 새로 정의되는 부분 수정 [TCPAI-1]

--- llama_three_test/repository/llama_three_repository_impl.py
+++ llama_three_test/repository/llama_three_repository_impl.py
@@ -7,6 +7,21 @@ from llama_three_test.repository.llama_three_repository import LlamaThreeReposit
 class LlamaThreeRepositoryImpl(LlamaThreeRepository):
     __instance = None
 
+    tokenizer = AutoTokenizer.from_pretrained(
+        "MLP-KTLim/llama-3-Korean-Bllossom-8B-gguf-Q4_K_M")
+
+    model = Llama(
+        model_path="/Users/j213h/Downloads/llama-3-Korean-Bllossom-8B-Q4_K_M.gguf",
+        n_ctx=512,
+        n_gpu_layers=-1
+    )
+    model.verbose = False  # make model silent
+
+    systemPrompt = """
+                당신은 유용한 AI 어시스턴트입니다. 사용자의 질의에 대해 친절하고 정확하게 답변해야 합니다.
+                You are a helpful AI assistant, you'll need to answer users' queries in a friendly and accurate manner.
+            """
+
     def __new__(cls):
         if cls.__instance is None:
             cls.__instance = super().__new__(cls)
@@ -21,27 +36,12 @@ class LlamaThreeRepositoryImpl(LlamaThreeRepository):
         return cls.__instance
 
     def generateText(self, userSendMessage):
-
-        tokenizer = AutoTokenizer.from_pretrained(
-            "MLP-KTLim/llama-3-Korean-Bllossom-8B-gguf-Q4_K_M")
-
-        model = Llama(
-            model_path="/Users/j213h/Downloads/llama-3-Korean-Bllossom-8B-Q4_K_M.gguf",
-            n_ctx=512,
-            n_gpu_layers=-1
-        )
-
-        systemPrompt = """
-            당신은 유용한 AI 어시스턴트입니다. 사용자의 질의에 대해 친절하고 정확하게 답변해야 합니다.
-            You are a helpful AI assistant, you'll need to answer users' queries in a friendly and accurate manner.
-        """
-
         messages = [
-            { "role": "system", "content": f"{systemPrompt}" },
+            { "role": "system", "content": f"{self.systemPrompt}" },
             { "role": "user", "content": f"{userSendMessage}" }
         ]
 
-        prompt = tokenizer.apply_chat_template(
+        prompt = self.tokenizer.apply_chat_template(
             messages,
             tokenize=False,
             add_generation_prompt=True
@@ -55,7 +55,7 @@ class LlamaThreeRepositoryImpl(LlamaThreeRepository):
             "echo": True
         }
 
-        response = model(prompt, **generationKwargs)
+        response = self.model(prompt, **generationKwargs)
         generatedText = response['choices'][0]['text'][len(prompt):]
 
         return { "generatedText": generatedText }
