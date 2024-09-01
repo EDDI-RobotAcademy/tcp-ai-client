ad667c9ef116b71787be02c2eea46fa602e1fb8e
Jaehyuk Han <jh.jaehyuk213@gmail.com>
2024-09-01 18:59:19 +0900

    fix: [한재혁] RAG를 pipeline를 사용하지 않도록 구성 변경 [TCPAI-5]

--- llama_three_test/repository/llama_three_repository_impl.py
+++ llama_three_test/repository/llama_three_repository_impl.py
@@ -1,8 +1,10 @@
 import os
 
+import numpy as np
 import torch
 from langchain.chains.retrieval_qa.base import RetrievalQA
 from langchain_community.llms.huggingface_pipeline import HuggingFacePipeline
+from langchain_core import documents
 from tqdm import tqdm
 from transformers import AutoTokenizer, AutoModel, pipeline
 from llama_cpp import Llama
@@ -30,13 +32,6 @@ class LlamaThreeRepositoryImpl(LlamaThreeRepository):
                 당신은 유용한 AI 어시스턴트입니다. 사용자의 질의에 대해 친절하고 정확하게 답변해야 합니다.
                 You are a helpful AI assistant, you'll need to answer users' queries in a friendly and accurate manner.
             """
-
-    if torch.cuda.is_available():
-        DEVICE = "cuda"
-    elif torch.backends.mps.is_available():
-        DEVICE = "mps"
-    else:
-        DEVICE = "cpu"
 
     def __new__(cls):
         if cls.__instance is None:
@@ -76,20 +71,15 @@ class LlamaThreeRepositoryImpl(LlamaThreeRepository):
         #
         # return { "generatedText": generatedText }
 
-        modelPipeline = pipeline(
-            "text-generation",
-            model="models/llama-3-Korean-Bllossom-8B-Q4_K_M.gguf",
-            tokenizer=self.tokenizer,
-            device=self.DEVICE
-        )
-        llm = HuggingFacePipeline(pipeline=modelPipeline)
+        queryEmbedding = self.model.embed(userSendMessage)
 
-        qa = RetrievalQA.from_chain_type(
-            llm=llm,
-            chian_type="stuff",
-            retriver=vectorstore.as_retriever()
-        )
+        distances, indices = vectorstore.search(np.array([queryEmbedding]), 5)
 
-        response = qa.run(userSendMessage)
+        retrievedDocs = [documents[i] for i in indices[0]]
 
-        return { "response": response }
+        context = " ".join(retrievedDocs)
+        prompt = f"주어진 Context의 내용을 바탕으로 질문에 대답하세요. 모르는 내용이 나오면 검색하거나 모른다고 대답하세요.\n\nContext: {context}\nQuestion: {queryEmbedding}\nAnswer:"
+
+        output = self.model(prompt, max_tokens=512, temperature=0.3)
+
+        return { "generatedText": output['choices'][0]['text'] }
