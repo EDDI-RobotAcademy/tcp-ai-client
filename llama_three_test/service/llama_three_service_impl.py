6b1a468681a360d81863fde7fb25537543e558ca
y0ng98 <dldydgnl2080@gmail.com>
2024-09-05 10:09:06 +0900

    fix: [이용휘] keyword 제공을 위해 userSendMessage 수정 [TCPAI-5]

--- llama_three_test/service/llama_three_service_impl.py
+++ llama_three_test/service/llama_three_service_impl.py
@@ -40,13 +40,14 @@ class LlamaThreeServiceImpl(LlamaThreeService):
             DOWNLOAD_PATH = "download_pdfs"
             FILE_PATH = os.path.join(os.getcwd(), DOWNLOAD_PATH, fileKey)
 
-            await self.__preprocessingRepository.downloadFileFromS3(fileKey)
-            print("finish to download file")
+            if not os.path.exists(FILE_PATH):
+                await self.__preprocessingRepository.downloadFileFromS3(fileKey)
+                print("finish to download file")
+
             text = self.__preprocessingRepository.extractTextFromPDFToMarkdown(FILE_PATH)
             print("finish to extract pdf to markdown")
 
             mainText, _ = self.__preprocessingRepository.separateMainAndReferences(text)
-            print(f"mainText: {mainText}")
             print("finish to separate main and references")
 
             documentList = self.__preprocessingRepository.splitTextIntoDocuments(mainText)
